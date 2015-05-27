1c1c6e2f93a9b47918af2090cd656e6944e14b5e
Abner Yang <abtyang@gmail.com>
2015-05-27 12:39:05 -0400

    api access functionality

---
+++ api-testing/console.rb
@@ -0,0 +1,21 @@
+require "httparty"
+
+options = {
+  :headers => {
+    "ACCEPT" => "application/json",
+    "CONTENT-TYPE" => "application/json",
+    "x-api-key" => "78861666c8ba"
+    # :body => {
+    #   chef_login: { 
+    #     login: "sunsheeppoplar",
+    #     password: "chipotleswrath"
+    #   }
+    } # This is what you’re missing
+}
+# response = HTTParty.post("http://www.weeatt.com/api/v1/chefs/sign_in", options)
+
+# puts response
+
+response = HTTParty.get("http://www.weeatt.com/api/v1/recipes?qs=okra&auth_token=leh0XQY71OGO0iFY_6aJ", options)
+
+puts response["results"][0]["instructions"]
