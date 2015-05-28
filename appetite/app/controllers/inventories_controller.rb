d554feb7fd1777457c47b2792168113056b95d93
Your Name <you@example.com>
2015-05-28 16:18:58 -0400

    inventories controller

--- appetite/app/controllers/inventories_controller.rb
+++ appetite/app/controllers/inventories_controller.rb
@@ -1,12 +1,30 @@
 class InventoriesController < ApplicationController
 	protect_from_forgery with: :null_session
 
+	def index
+	@user = User.find(session[:user_id])
+	@inventories =  Inventory.where(user_id: session[:user_id]).joins(:recipes)
+	# respond_to do |format|
+	# 		format.html { render :index }
+	# 		format.json { render json: @inventory.to_json}
+	if @inventories
+	render json: @inventories.to_json(:include => [:inventory_recipe])
+		end
+	end
+
+
+
+	def show
+	end
+
+
 	def create
 	end
 
 	def update
 	end
 
-	def destory
+	def destroy
 	end
+
 end
--- appetite/app/controllers/recipes_controller.rb
+++ appetite/app/controllers/recipes_controller.rb
@@ -1,9 +1,20 @@
 class RecipesController < ApplicationController
 	protect_from_forgery with: :null_session
 
+
+	def index
+	end
+
+	def show
+	end
+
+
 	def create
 	end
 
+	def update
+	end
+	
 	def destroy
 	end
 
