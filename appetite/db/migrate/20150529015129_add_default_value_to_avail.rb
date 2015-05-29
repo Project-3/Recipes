ffbfb5ecd6877275ae7d16504e82b1be0ee326d1
Your Name <you@example.com>
2015-05-28 22:19:20 -0400

    finished inventory controller hopefully

---
+++ appetite/db/migrate/20150529015129_add_default_value_to_avail.rb
@@ -0,0 +1,10 @@
+class AddDefaultValueToAvail < ActiveRecord::Migration
+	# add default true value to new inventory
+	def up
+		change_column :inventories, :avail, :boolean, :default => true
+	end
+
+	def down
+		change_column :inventories, :avail, :boolean, :default => nil
+	end
+end
