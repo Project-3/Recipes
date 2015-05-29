1565d08e8a30d6df8f6383c9b47ebe00b534e638
Your Name <you@example.com>
2015-05-29 11:21:52 -0400

    clean up

--- appetite/app/controllers/inventories_controller.rb
+++ appetite/app/controllers/inventories_controller.rb
@@ -1,7 +1,6 @@
 class InventoriesController < ApplicationController
 	protect_from_forgery with: :null_session
 
-<<<<<<< HEAD
 	# gets all of the users ingredients and any user recipes associated with that ingredient
 	def index
 		@user = User.find(session[:user_id])
@@ -12,22 +11,8 @@ class InventoriesController < ApplicationController
 			flash[:error] = "Start out by adding what food you have on hand to your inventory (:^J)"
 			render status: 400, nothing: true
 		end
-=======
-
-# gets all of the users ingredients and any user recipes associated with that ingredient
-def index
-	@user = User.find(session[:user_id])
-	@inventories =  @user.inventories
-	if @inventories
-		render json: @inventories.to_json(:include => [:recipes])
-	else
-		flash[:error] = "Start out by adding what food you have on hand to your inventory (:^J)"
-		render status: 400, nothing: true
->>>>>>> nat_debug2
 	end
-end
 
-<<<<<<< HEAD
 	#gets the saved recipes associated with any one ingredient
 	def show
 		@inventory =  Inventory.find(params[:id])
@@ -69,47 +54,4 @@ end
 	def inventory_params
 		params.require(:inventory).permit(:ingredient, :group, :user_id)
 	end
-=======
-#gets the saved recipes associated with any one ingredient
-def show
-	@inventory =  Inventory.find(params[:id])
-	if @inventories
-		render json: @inventory.to_json(:include => [:recipes])
-	end
-end
-
-# create new ingredient, save it, let backbone rerender its view
-
-### added default value to the avail column so when you enter an ingredient it will be 'true'
-def create
-	@inventory = Inventory.new(inventory_params)
-	if @inventory.save
-		render json: @inventory
-	end
-end
-
-def update
-	@inventory = Inventory.find(params[:id])
-	if @inventory.update(inventory_params)
-		render json: @inventory
-	else
-		render status: 400, nothing: true
-	end
-end
-
-
-def destroy
-	@inventory = Inventory.find(params[:id])
-	if @inventory.destroy
-		render json: {}
-	else
-		render status: 400, nothing: true
-	end
-end
-
-private
-def inventory_params
-	params.require(:inventory).permit(:ingredient, :group, :user_id)
-end
->>>>>>> nat_debug2
 end
