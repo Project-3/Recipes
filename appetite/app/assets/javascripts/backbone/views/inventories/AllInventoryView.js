cf08ff63025edc6c717f6eb60d94a10042962480
Abner Yang <abtyang@gmail.com>
2015-05-29 15:32:59 -0400

    inventory template work

--- appetite/app/assets/javascripts/backbone/views/inventories/AllInventoryView.js
+++ appetite/app/assets/javascripts/backbone/views/inventories/AllInventoryView.js
@@ -1,12 +1,31 @@
 Appetite.Views.AllInventoryView = Backbone.View.extend({
 	el: "div#content",
-	initialize: function() {
+	initialize: function() { 
 		this.listenTo(this.collection, "sync remove", this.render);
+
 	},
 
+
 	render: function() {
+		console.log(this.collection.models)
 		var div = this.$el;
 		div.html("");
+
+
+		var protein = []
+		var produce = []
+		var dairy = []
+		var grain = []
+
+		this.collection.models.each(function(inventory) {
+				console.log(collection)
+			if (this.collection.models.attributes === "protein") {
+
+				protein.push(this.collection.models)
+				console.log(protein)
+			}
+
+		})
 
 		this.collection.each(function(inventory) {
 			div.append(new Appetite.Views.InventoryView({model: inventory}).render().$el);
