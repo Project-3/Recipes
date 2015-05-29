dfa9c11e926e6d63ade5d090c803eb20a2df31fd
kwokster10 <kwokster10@hotmail.com>
2015-05-29 15:32:52 -0400

    filters for inventory

--- appetite/app/assets/javascripts/backbone/collections/inventories.js
+++ appetite/app/assets/javascripts/backbone/collections/inventories.js
@@ -10,5 +10,21 @@ Appetite.Collections.Inventories = Backbone.Collection.extend({
   url: "/user/inventories",
   defaults: {
   	avail: true
+  },
+  // filter for only protein group
+  protein: function() {
+  	return this.where({group: "protein"});
+  },
+  // filter for only produce 
+  produce: function() {
+  	return this.where({group: "produce"});
+  },
+  // filter for only dairy
+  dairy: function() {
+  	return this.where({group: "dairy"});
+  },
+  // filter for only grain
+  grain: function() {
+  	return this.where({group: "grain"});
   }
 });
