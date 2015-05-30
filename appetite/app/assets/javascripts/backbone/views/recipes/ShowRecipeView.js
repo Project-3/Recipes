32fbe62eb65f47d7fe25a34633bf7dc3a5cb7f8a
Your Name <you@example.com>
2015-05-29 20:23:36 -0400

    recipes show page works

--- appetite/app/assets/javascripts/backbone/views/recipes/ShowRecipeView.js
+++ appetite/app/assets/javascripts/backbone/views/recipes/ShowRecipeView.js
@@ -6,26 +6,29 @@ var Appetite = Appetite || {
 };
 
 Appetite.Views.ShowRecipeView = Backbone.View.extend({
-	tagName: "div",
+	el: "#content",
 
 	initialize: function(){
-	 	this.template = _.template($("#show-recipe").html());
-	 	this.render();
+		this.template = _.template($("#show-recipe").html());
 	},
 
 	events: {},
 	
-	recipeShow: function(){
+	// recipeShow: function(){
 		
 
-	},
+	// },
 
 	deleteRecipe: function(){
 		this.model.destroy();
 	},
 
 	render: function(){
+		// var recipe = this.model.toJSON()
+		// var template = _.template($("#show-recipe").html(),{recipe: recipe});
+		// console.log(this.model)
 		this.$el.html(this.template({recipe: this.model.toJSON()}));
+		console.log(this.model)
 		return this;
 	}
 	
